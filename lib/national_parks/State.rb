9f8097de866413003b20a3e537534e4813f0902a
heislercreative <30783939+heislercreative@users.noreply.github.com>
2018-03-01 19:29:17 +0000

    define #scrape_states

--- lib/national_parks/State.rb
+++ lib/national_parks/State.rb
@@ -9,16 +9,22 @@ class NationalParks::State
   def self.scrape_states
     states = []
 
-    doc = Nokogiri::HTML(open("https://www.nps.gov/findapark/index.htm"))
+    states_page = Nokogiri::HTML(open("https://www.nps.gov/findapark/index.htm"))
 
-    binding.pry
+    state_list = states_page.css('area')
+    state_list.each do |state|
+      state_name = "#{state.attr("alt")}"
+      state_url = "#{state.attr("href")}"
+      
+      states << {name: state_name, url: state_url}
+    end
     # state_1 = self.new
     # state_1.name = "Alabama"
-    # state_1.url = "https://www.nps.gov/state/al/index.htm"
+    # state_1.url = "/state/al/index.htm"
     #
     # state_2 = self.new
     # state_2.name = "Alaska"
-    # state_2.url = "https://www.nps.gov/state/ak/index.htm"
+    # state_2.url = "/state/ak/index.htm"
 
     states
   end

25583983e07396271b4f3fac7361ee05b2d00d91
heislercreative <brennan@heislercreative.com>
2018-02-28 19:55:22 -0700

    added pry & nokogiri

--- lib/national_parks.rb
+++ lib/national_parks.rb
@@ -1,3 +1,6 @@
+require "pry"
+require "nokogiri"
+
 require_relative "./national_parks/version"
 require_relative "./national_parks/cli"
 require_relative "./national_parks/state"
--- lib/national_parks/State.rb
+++ lib/national_parks/State.rb
@@ -7,6 +7,7 @@ class NationalParks::State
   end
 
   def self.scrape_states
+    states = []
     state_1 = self.new
     state_1.name = "Alabama"
     state_1.url = "https://www.nps.gov/state/al/index.htm"
@@ -15,7 +16,7 @@ class NationalParks::State
     state_2.name = "Alaska"
     state_2.url = "https://www.nps.gov/state/ak/index.htm"
 
-    [state_1, state_2]
+    states
   end
 
 end

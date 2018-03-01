7b20b7eedde26cb906a5b4dd80588f49939494cb
heislercreative <brennan@heislercreative.com>
2018-02-28 19:17:20 -0700

    created State class

--- lib/national_parks.rb
+++ lib/national_parks.rb
@@ -1,2 +1,3 @@
 require_relative "./national_parks/version"
 require_relative "./national_parks/cli"
+require_relative "./national_parks/state"
--- lib/national_parks/State.rb
+++ lib/national_parks/State.rb
@@ -1,7 +1,10 @@
 class NationalParks::State
 
   def self.all
-
+    puts <<-DOC.gsub /^\s*/, ''
+      1. Alabama
+      2. Alaska
+    DOC
   end
-  
+
 end

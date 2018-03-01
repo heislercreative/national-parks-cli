cfaf653489b05703c479ff43b3edaf3cced36d74
heislercreative <brennan@heislercreative.com>
2018-02-28 20:00:00 -0700

    set first scrape URL

--- lib/national_parks.rb
+++ lib/national_parks.rb
@@ -1,5 +1,6 @@
-require "pry"
+require "open-uri"
 require "nokogiri"
+require "pry" #remove before publishing?
 
 require_relative "./national_parks/version"
 require_relative "./national_parks/cli"
--- lib/national_parks/State.rb
+++ lib/national_parks/State.rb
@@ -8,13 +8,17 @@ class NationalParks::State
 
   def self.scrape_states
     states = []
-    state_1 = self.new
-    state_1.name = "Alabama"
-    state_1.url = "https://www.nps.gov/state/al/index.htm"
 
-    state_2 = self.new
-    state_2.name = "Alaska"
-    state_2.url = "https://www.nps.gov/state/ak/index.htm"
+    doc = Nokogiri::HTML(open("https://www.nps.gov/findapark/index.htm"))
+
+    binding.pry
+    # state_1 = self.new
+    # state_1.name = "Alabama"
+    # state_1.url = "https://www.nps.gov/state/al/index.htm"
+    #
+    # state_2 = self.new
+    # state_2.name = "Alaska"
+    # state_2.url = "https://www.nps.gov/state/ak/index.htm"
 
     states
   end

0ee97b7972b15eb73b275afbf6dc25e605ff4492
heislercreative <brennan@heislercreative.com>
2018-02-28 18:59:48 -0700

    defined #state_menu

--- lib/national_parks/CLI.rb
+++ lib/national_parks/CLI.rb
@@ -2,15 +2,28 @@
 class NationalParks::CLI
 
   def call
-    puts "Welcome to the National Park Service!"
     list_parks
+    state_menu
   end
 
   def list_parks
+    puts "Welcome to the National Park Service!"
     puts <<-DOC.gsub /^\s*/, ''
       1. Alabama
       2. Alaska
     DOC
   end
+
+  def state_menu
+    puts "Please pick a US state or territory by number:"
+    input = gets.strip
+    case input
+    when "1"
+      puts "List of Alabama Parks"
+    when "2"
+      puts "List of Alaska Parks"
+    end
+  end
+
 
 end

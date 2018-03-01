45e49c09bb1e6ed918e5321c0307d42b30142a4d
heislercreative <brennan@heislercreative.com>
2018-02-28 18:53:20 -0700

    defined #list_parks

--- lib/national_parks/CLI.rb
+++ lib/national_parks/CLI.rb
@@ -3,6 +3,14 @@ class NationalParks::CLI
 
   def call
     puts "Welcome to the National Park Service!"
+    list_parks
+  end
+
+  def list_parks
+    puts <<-DOC
+      1. Alabama
+      2. Alaska
+    DOC
   end
 
 end

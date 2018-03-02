3bc57a0baf6d5c857a6e78af215e4e4c996b0cee
heislercreative <30783939+heislercreative@users.noreply.github.com>
2018-03-02 00:44:07 +0000

    Stub out parks URL to be scraped

--- lib/national_parks/park.rb
+++ lib/national_parks/park.rb
@@ -5,5 +5,11 @@ class NationalParks::Park
     #Scrape state parks page and return park names, types, locations, & descriptions
     self.scrape_parks
   end
+  def self.scrape_parks
+    parks = []
+
+    parks_page = Nokogiri::HTML(open("https://www.nps.gov/state/co/index.htm")) #replace with interpolated state url
+    puts parks_page
+  end
 
 end

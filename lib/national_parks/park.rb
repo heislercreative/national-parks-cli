72cca45477dcd7e7959d5bb3073705d349a54489
heislercreative <30783939+heislercreative@users.noreply.github.com>
2018-03-02 00:49:42 +0000

    Scrape & add park objects to array

--- lib/national_parks/park.rb
+++ lib/national_parks/park.rb
@@ -9,7 +9,18 @@ class NationalParks::Park
     parks = []
 
     parks_page = Nokogiri::HTML(open("https://www.nps.gov/state/co/index.htm")) #replace with interpolated state url
-    puts parks_page
+    
+    park_list = parks_page.css("div.col-md-9.col-sm-9.col-xs-12.table-cell.list_left")
+    park_list.each do |park|
+      park_type = park.css("h2").text
+      park_name = park.css("h3").text
+      park_location = park.css("h4").text
+      park_description = park.css("p").text
+      
+      parks << {name: park_name, type: park_type, location: park_location, description: park_description}
+    end
+    
+  puts parks
   end
 
 end

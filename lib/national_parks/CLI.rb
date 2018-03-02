53d1957c119afa3cefc118fe70fb202e95a9282b
heislercreative <brennan@heislercreative.com>
2018-03-02 16:27:33 -0700

    parks command functioning

--- lib/national_parks/CLI.rb
+++ lib/national_parks/CLI.rb
@@ -25,6 +25,7 @@ class NationalParks::CLI
   # State Methods #
   def list_states
     @states = NationalParks::State.all
+    puts ""
     @states.each.with_index(1) {|state, i| puts " #{i}. #{state[:name]}"}
     state_menu
   end
@@ -36,7 +37,8 @@ class NationalParks::CLI
     input = gets.strip.downcase
 
     if input.to_i > 0
-      list_parks(input.to_i-1)
+      @state_i = input.to_i-1
+      list_parks(@state_i)
     elsif input == "exit"
       goodbye
     else
@@ -49,6 +51,7 @@ class NationalParks::CLI
   def list_parks(choice)
     @chosen_state = BASE_PATH + @states[choice][:url]
 
+    puts ""
     puts "==#{NationalParks::Park.state_name(@chosen_state)}=="
     @parks = NationalParks::Park.all(@chosen_state)
     @parks.each.with_index(1) {|park, i| puts " #{i}. #{park[:name]}"}
@@ -67,15 +70,17 @@ class NationalParks::CLI
       if input.to_i > 0
         i = input.to_i-1
         puts ""
+        puts "=================================================="
+        puts ""
         puts "#{@parks[i][:name].upcase} #{@parks[i][:type].upcase}"
         puts "-#{@parks[i][:location]}-"
         puts "#{@parks[i][:description]}"
         puts ""
         puts "  - Type 'parks' to return to the parks list."
         puts "  - Type 'states' to return to the states list."
-        puts "  - Type 'exit' to leave."
+        puts "  - Type 'exit' to leave"
       elsif input == "parks"
-        list_parks(choice)
+        list_parks(@state_i)
       elsif input == "states"
         list_states
       elsif input == "exit"

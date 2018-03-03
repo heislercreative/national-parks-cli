e20f77b1360f3b4dab01e747a69e3cac6e9ce8de
Brennan Heisler <learn@heislercreative.com>
2018-03-03 09:21:08 +0000

    Capitalize CLI class required

--- lib/national_parks.rb
+++ lib/national_parks.rb
@@ -2,6 +2,6 @@ require "open-uri"
 require "nokogiri"
 
 require_relative "./national_parks/version"
-require_relative "./national_parks/cli"
+require_relative "./national_parks/CLI"
 require_relative "./national_parks/state"
 require_relative "./national_parks/park"
